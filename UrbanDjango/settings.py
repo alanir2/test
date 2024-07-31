6adc466bea492ed0530fd27c4611991736fc5079
alanir2 <156774124+alanir2@users.noreply.github.com>
2024-07-31 20:56:55 +0300

    Update settings.py
    классовое представление

--- UrbanDjango/settings.py
+++ UrbanDjango/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task2.apps.Task2Config',
+    'task2',
     'task3',
     'task4'
 ]
