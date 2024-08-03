4871466da4a65eb1aa1034c93ab8269fd353ed0b
alanir2 <156774124+alanir2@users.noreply.github.com>
2024-08-03 20:48:39 +0300

    Update views.py

--- task2/views.py
+++ task2/views.py
@@ -7,7 +7,7 @@ from django.template.loader import render_to_string
 
 def index(request):
 
-    return render(request, 'second_task/func_template.http')
+    return render(request, 'second_task/func_template.html')
 
 class index2(TemplateView):
     template_name = 'class_template.html'
