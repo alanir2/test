c2c6caac1a42234ae670311f057feb996259476d
alanir2 <156774124+alanir2@users.noreply.github.com>
2024-08-03 11:43:07 +0300

    Update urls.py

--- task2/urls.py
+++ task2/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path, include
-from . import views
+from task2.views import index, index2
+
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', views.index),
-    path('cat/', views.categories)
+    path('admin/', admin.site.urls),
+    path('', index),
+    path('index/', index2.as_view())
 ]
